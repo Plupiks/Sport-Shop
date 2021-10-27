c9660ed4dde9cacc644b9f4e35559e1f70f87a3c
Vitaliy Vasylykiv <92222356+Plupiks@users.noreply.github.com>
2021-10-27 18:05:50 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+
+  slidesPerView: 4,
+  spaceBetween: 10,
+});
